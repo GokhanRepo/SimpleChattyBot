7e38348a1c20985603cd248a6b1b94b1fb1d964c
GokhanRepo <mrk.rckfrd@gmail.com>
2023-01-16 18:42:14 +0300

    Started creating Simple Chatty Bot

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,18 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        String yourName = scanner.nextLine();
+
+        System.out.println("What a great name you have," + yourName + "!");
+    }
+}
