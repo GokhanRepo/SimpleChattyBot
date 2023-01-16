df3ff3ac0df6fd02f069f8a6cfc5e6ebd5c2415e
GokhanRepo <mrk.rckfrd@gmail.com>
2023-01-16 19:31:39 +0300

    Simple Chatty Bot: age guessing game

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -10,9 +10,18 @@ public class SimpleBot {
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        // reading a name
-        String yourName = scanner.nextLine();
+        String name = scanner.nextLine();
 
-        System.out.println("What a great name you have," + yourName + "!");
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt() ;
+        int remainder7 = scanner.nextInt();
+        int yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is "+yourAge+"; that's a good time to start programming!");
     }
 }
